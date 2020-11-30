c353c7386b61bab98e61c3608728f72d03783750
MatthewPrinz <mattprinz@gmail.com>
2020-11-30 01:29:18 -0600

    Iterating through string instead of through hashmap (faster butstill O(N)

--- first-unique-character-in-a-string.java
+++ first-unique-character-in-a-string.java
@@ -1,29 +1,37 @@
 class Solution {
-    public int firstUniqChar(String s) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++)
-        {
-            if (map.containsKey(s.charAt(i)))
-            {
-                int prev = map.get(s.charAt(i));
-                map.put(s.charAt(i), prev+1);
-            }
-            else
-            {
-                map.put(s.charAt(i), 1);
-            }
-        }
-        int minPos = Integer.MAX_VALUE;
-        for (Map.Entry<Character, Integer> me : map.entrySet())
-        {
-            if (me.getValue() < 2)
-            {
-                if (s.indexOf(me.getKey()) < minPos)
-                    minPos = s.indexOf(me.getKey());
-            }
-        }
-        if (minPos < Integer.MAX_VALUE)
-            return minPos;
-        return -1;
-    }
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (map.containsKey(s.charAt(i)))
+            {
+                int prev = map.get(s.charAt(i));
+                map.put(s.charAt(i), prev+1);
+            }
+            else
+            {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (map.get(s.charAt(i)) < 2)
+            {
+                return i;
+            }
+        }
+        return -1;
+        // int minPos = Integer.MAX_VALUE;
+        // for (Map.Entry<Character, Integer> me : map.entrySet())
+        // {
+        //     if (me.getValue() < 2)
+        //     {
+        //         if (s.indexOf(me.getKey()) < minPos)
+        //             minPos = s.indexOf(me.getKey());
+        //     }
+        // }
+        // if (minPos < Integer.MAX_VALUE)
+        //     return minPos;
+        // return -1;
+    }
 }
