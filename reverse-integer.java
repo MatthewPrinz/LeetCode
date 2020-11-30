f186285420fab639076ddf51898245c9a204032c
MatthewPrinz <mattprinz@gmail.com>
2020-11-29 20:03:19 -0600

    working commit - Created using LeetHub

---
+++ reverse-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int reverse(int x) {
+        boolean negative = false;
+        int input = x;
+        if (x < 0)
+        {
+            negative = true;
+            x *= -1;
+        }
+        String intx = Integer.toString(x);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < intx.length(); i++)
+        {
+            sb.append(intx.charAt(intx.length()-i-1));            
+        }
+        Integer result = 0;
+        try {
+         result = Integer.parseInt(sb.toString());
+            }
+        catch (Exception e)
+        {
+            return 0;
+        }
+        if (negative)
+            result *= -1;
+        return result;
+    }
+}
