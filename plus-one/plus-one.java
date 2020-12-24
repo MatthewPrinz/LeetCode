d92bcc9bf7823dc24291d2cf9f6e9a6c3d19b7be
MatthewPrinz <mattprinz@gmail.com>
2020-12-24 02:44:22 -0600

    Time: 0 ms (100.00%), Space: 37.5 MB (67.41%) - LeetHub

--- plus-one/plus-one.java
+++ plus-one/plus-one.java
@@ -1,34 +1,46 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        List<Integer> list = new ArrayList<>();
-        boolean carry = false;
-        for (int i = digits.length - 1; i >= 0; i--)
-        {
-            if (i == digits.length - 1)
-            {
+        int n = digits.length;
+        for(int i=n-1; i>=0; i--) {
+            if(digits[i] < 9) {
                 digits[i]++;
+                return digits;
             }
-            if (carry)
-            {
-                digits[i]++;
-                carry = false;
-            }
-            if (digits[i] >= 10)
-            {
-                digits[i] -= 10;
-                list.add(digits[i]);
-                carry = true;
-            }
-            else
-            {
-                list.add(digits[i]);
-            }
+​
+            digits[i] = 0;
         }
-        if (carry)
-        {
-            list.add(1);
-        }
-        Collections.reverse(list);
-        return list.stream().mapToInt(i->i).toArray();
+        int[] newNumber = new int [n+1];
+        newNumber[0] = 1;
+        return newNumber;
+        // List<Integer> list = new ArrayList<>();
+        // boolean carry = false;
+        // for (int i = digits.length - 1; i >= 0; i--)
+        // {
+        //     if (i == digits.length - 1)
+        //     {
+        //         digits[i]++;
+        //     }
+        //     if (carry)
+        //     {
+        //         digits[i]++;
+        //         carry = false;
+        //     }
+        //     if (digits[i] >= 10)
+        //     {
+        //         digits[i] -= 10;
+        //         list.add(digits[i]);
+        //         carry = true;
+        //     }
+        //     else
+        //     {
+        //         list.add(digits[i]);
+        //     }
+        // }
+        // if (carry)
+        // {
+        //     list.add(1);
+        // }
+        // Collections.reverse(list);
+        // return list.stream().mapToInt(i->i).toArray();
     }
 }
