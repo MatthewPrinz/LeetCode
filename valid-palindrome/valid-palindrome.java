d5ccbbedcbd8c720b4edecaec9eca256bba45457
MatthewPrinz <mattprinz@gmail.com>
2020-12-27 16:41:58 -0600

    Time: 72 ms (8.45%), Space: 40 MB (27.26%) - LeetHub

---
+++ valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String sl = s.toLowerCase();
+        if (sl.length() == 0)
+            return true;
+        int beginPos = 0;
+        int endPos = sl.length()-1;
+        while (beginPos < endPos)
+        {
+            if (!Character.isLetterOrDigit((sl.charAt(beginPos))) || !Character.isLetterOrDigit(sl.charAt(beginPos)))
+            {
+                beginPos++;
+                continue;
+            }
+            if (!Character.isLetterOrDigit((sl.charAt(endPos))) || !Character.isLetterOrDigit(sl.charAt(endPos)))
+            {
+                endPos--;
+                continue;
+            }
+            System.out.println("begin: " + sl.charAt(beginPos) + " end: " + sl.charAt(endPos));
+            if (sl.charAt(beginPos) != sl.charAt(endPos))
+                return false;
+            beginPos++;
+            endPos--;
+        }
+    return true;
+    }
+}
