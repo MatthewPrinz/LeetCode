c900a0cc7ca94031fdd269f1a78a78f407dfad46
MatthewPrinz <mattprinz@utexas.edu>
2021-06-25 22:14:11 -0500

    Time: 3 ms (99.67%), Space: 42.3 MB (86.48%) - LeetHub

--- contains-duplicate/contains-duplicate.java
+++ contains-duplicate/contains-duplicate.java
@@ -1,12 +1,10 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> numset = new HashSet<>();
-        for (int i = 0; i < nums.length; i++)
-        {
-            if (numset.contains(nums[i]))
-                return true;
-            numset.add(nums[i]);
-        }
-        return false;
-    }
+    public boolean containsDuplicate(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 1; ++i) {
+      if (nums[i] == nums[i + 1])
+        return true;
+    }
+    return false;   
+    }
 }
