c824b184c1c7d79bba27fb497852d6b215a5fd31
MatthewPrinz <mattprinz@utexas.edu>
2021-08-21 00:03:56 -0500

    Time: 61 ms (5.08%), Space: 38.7 MB (98.23%) - LeetHub

--- maximum-units-on-a-truck/maximum-units-on-a-truck.java
+++ maximum-units-on-a-truck/maximum-units-on-a-truck.java
@@ -1,34 +1,30 @@
 class Solution {
-    public int maximumUnits(int[][] boxTypes, int truckSize) {
-        int result = 0;
-​
-        for (int j = 0; j < boxTypes.length; j++)
-        {
-            int maxUnits = -1;
-            int maxIndex = -1;
-            for (int i = 0; i < boxTypes.length; i++)
-            {
-                if (boxTypes[i][1] > maxUnits && boxTypes[i][0] > 0)
-                {
-                    maxUnits = boxTypes[i][1];
-                    maxIndex = i;
-                }
-            }
-            System.out.println("maxIndex = " + maxIndex);
-            if (truckSize > boxTypes[maxIndex][0])
-            {
-                truckSize -= boxTypes[maxIndex][0];
-                result += boxTypes[maxIndex][1] * boxTypes[maxIndex][0];
-                boxTypes[maxIndex][0] = 0;
-                System.out.println("boxTypes[maxIndex][0]: " + boxTypes[maxIndex][0]);
-            }
-            else
-            {
-                result += truckSize * boxTypes[maxIndex][1];
-                break;
-            }
-            System.out.println("truckSize: " + truckSize + " result: " + result);
-        }
-        return result;
-    }
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        int result = 0;
+        for (int j = 0; j < boxTypes.length; j++)
+        {
+            int maxUnits = -1;
+            int maxIndex = -1;
+            for (int i = 0; i < boxTypes.length; i++)
+            {
+                if (boxTypes[i][1] > maxUnits && boxTypes[i][0] > 0)
+                {
+                    maxUnits = boxTypes[i][1];
+                    maxIndex = i;
+                }
+            }
+            if (truckSize > boxTypes[maxIndex][0])
+            {
+                truckSize -= boxTypes[maxIndex][0];
+                result += boxTypes[maxIndex][1] * boxTypes[maxIndex][0];
+                boxTypes[maxIndex][0] = 0;
+            }
+            else
+            {
+                result += truckSize * boxTypes[maxIndex][1];
+                break;
+            }
+        }
+        return result;
+    }
 }
