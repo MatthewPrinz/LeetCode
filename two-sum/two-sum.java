c89a562c8c360b8c5d45d30baa067a76328eb410
MatthewPrinz <mattprinz@utexas.edu>
2021-08-24 21:38:48 -0500

    Time: 4 ms (51.42%), Space: 40 MB (20.29%) - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,15 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] solution = new int[2];
         Map<Integer, Integer> valueToIndex = new HashMap<>();
+        int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
             if (valueToIndex.containsKey(target - nums[i])) {
-                solution[0] = i;
-                solution[1] = valueToIndex.get(target - nums[i]);
+                result[0] = i;
+                result[1] = valueToIndex.get(target - nums[i]);
             }
             valueToIndex.put(nums[i], i);
-
         }
-        return solution;
+        return result;
     }
 }
