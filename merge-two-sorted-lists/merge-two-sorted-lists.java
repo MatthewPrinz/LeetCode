8ca850f27f8b5c2056626ee64a31af4d528a959a
MatthewPrinz <mattprinz@utexas.edu>
2021-09-11 15:48:36 -0500

    Time: 2 ms (5.46%), Space: 40 MB (11.44%) - LeetHub

--- merge-two-sorted-lists/merge-two-sorted-lists.java
+++ merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,55 +1,59 @@
-        else
-        {
-            head = new ListNode(l1.val);
-            l1 = l1.next;
-        }
-        ListNode result = head;
-        while (l1 != null && l2 != null)
-        {
-            System.out.println("L1.val = " + l1.val + " l2.val: " + l2.val);
-            if (l1.val < l2.val)
-            {
-                head.next = new ListNode(l1.val);
-                l1 = l1.next;
-            }
-            else
-            {
-                head.next = new ListNode(l2.val);
-                l2 = l2.next;
-            }
-            head = head.next;
-        }
-        while (l1 != null)
-        {
-            head.next = new ListNode(l1.val);
-            head = head.next;
-            l1 = l1.next;
-        }
-        while (l2 != null)
-        {
-            head.next = new ListNode(l2.val);
-            head = head.next;
-            l2 = l2.next;
-        }
-        return result;
-        
-        // while (l1 != null || l2 != null)
-        // {
-        //     while (l2 != null && l1.val > l2.val)
-        //     {
-        //         System.out.println("In first while, l2 = " + l2.val);
-        //         head.next = new ListNode(l2.val);
-        //         head = head.next;
-        //         l2 = l2.next;
-        //     }
-        //     while (l1 != null && l2.val > l1.val)
-        //     {
-        //         System.out.println("In second while, l1 = " + l1.val);
-        //         head.next = new ListNode(l1.val);
-        //         head = head.next;
-        //         l1 = l1.next;
-        //     } 
-        // }
-        // return head;
-    }
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null)
+            return null;
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        ListNode head;
+        if (l1.val > l2.val)
+        {
+            head = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        else
+        {
+            head = new ListNode(l1.val);
+            l1 = l1.next;
+        }
+        ListNode result = head;
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val < l2.val)
+            {
+                head.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }
+            else
+            {
+                head.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        while (l1 != null)
+        {
+            head.next = new ListNode(l1.val);
+            head = head.next;
+            l1 = l1.next;
+        }
+        while (l2 != null)
+        {
+            head.next = new ListNode(l2.val);
+            head = head.next;
+            l2 = l2.next;
+        }
+        return result;
+    }
 }
