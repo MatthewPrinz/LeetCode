c030ff6edab2457c6c2543539f7c39baa87f9a4e
MatthewPrinz <mattprinz@utexas.edu>
2021-09-11 12:53:47 -0500

    Time: 5 ms (95.49%), Space: 41.4 MB (85.63%) - LeetHub

--- merge-intervals/merge-intervals.java
+++ merge-intervals/merge-intervals.java
@@ -4,24 +4,19 @@ class Solution {
             return new int[0][0];
         }
         Arrays.sort(intervals, (e1, e2) -> Integer.compare(e1[0], e2[0]));
-        List<Integer[]> result = new ArrayList<>();
+        List<int[]> result = new ArrayList<>();
         int newBegin = intervals[0][0];
         int newEnd = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
             if (newEnd >= intervals[i][0]) {
-                newEnd = newEnd > intervals[i][1] ? newEnd : intervals[i][1];
+                newEnd = Math.max(newEnd, intervals[i][1]);
             } else {
-                result.add(new Integer[]{newBegin, newEnd});
+                result.add(new int[]{newBegin, newEnd});
                 newBegin = intervals[i][0];
                 newEnd = intervals[i][1];
             }
         }
-        result.add(new Integer[]{newBegin, newEnd});
-        int[][] resultArr = new int[result.size()][2];
-        for (int i = 0; i < result.size(); i++) {
-            resultArr[i][0] = result.get(i)[0];
-            resultArr[i][1] = result.get(i)[1];
-        }
-        return resultArr;
+        result.add(new int[]{newBegin, newEnd});
+        return result.toArray(new int[result.size()][]);
     }
 }
