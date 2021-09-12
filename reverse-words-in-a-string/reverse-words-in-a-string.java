b4c6a2e97869159b95d070e3e3dff3a60d2c097e
MatthewPrinz <mattprinz@utexas.edu>
2021-09-12 02:18:34 -0500

    Time: 41 ms (5.04%), Space: 42.5 MB (5.72%) - LeetHub

---
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] split = s.split("\\s");
+        System.out.println(Arrays.toString(split));
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length - 1; i >= 0; i--) {
+            if (!split[i].isEmpty())
+                sb.append(split[i]).append(" ");
+        }
+        return sb.toString().substring(0, sb.length()-1);
+    }
+}
