6abb47df2730a8d3b3b577080f748d592e4a2288
MatthewPrinz <mattprinz@utexas.edu>
2021-09-12 02:52:38 -0500

    Time: 1 ms (99.98%), Space: 38.2 MB (11.44%) - LeetHub

--- power-of-two/power-of-two.java
+++ power-of-two/power-of-two.java
@@ -1,17 +1,14 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n == 1)
-            return true;
         return helper((double)n);
     }
     
-    public boolean helper(double n)
-    {
-        if (n == 2.0)
+    public boolean helper(double n){
+        if (n == 1.0)
             return true;
-        else if (n < 2.0)
+        else if (n < 1.0)
             return false;
         else 
-            return helper(n/(double)2.0);
+            return helper(n/2.0);
     }
 }
