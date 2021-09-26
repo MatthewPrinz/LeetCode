c38cdee995ce449ac3d5654b2e27990f76aa741d
MatthewPrinz <mattprinz@utexas.edu>
2021-09-25 20:31:50 -0500

    Time: 2 ms (11.16%), Space: 38.3 MB (23.80%) - LeetHub

---
+++ combination-sum-iii/combination-sum-iii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, k, n, new ArrayList<>(), 1);
+        return result;
+    }
+    
+    void backtrack(List<List<Integer>> result, int n, int target, List<Integer> temp, int start) {
+        if (target < 0 || n < 0)
+            return;
+        else if (target == 0 && n == 0)
+            result.add(new ArrayList<>(temp));
+        else {
+            for (int i = start; i <= 9; i++) {
+                if (temp.contains(i))
+                    continue;
+                temp.add(i);
+                backtrack(result, n-1, target - i, temp, i+1);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+}
