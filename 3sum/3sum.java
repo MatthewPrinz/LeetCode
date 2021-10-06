97202943921f509f46ed01fb6e5d379c1f82defa
MatthewPrinz <mattprinz@utexas.edu>
2021-10-05 21:52:30 -0500

    Time: 42 ms (39.43%), Space: 56.7 MB (23.03%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        if (nums.length == 0)
+        if (nums.length <= 2)
             return result;
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
