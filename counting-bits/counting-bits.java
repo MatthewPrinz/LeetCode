9d701120c1fcfbacc7337a40ad0e691a7b3c2e43
MatthewPrinz <mattprinz@utexas.edu>
2021-10-07 23:09:43 -0500

    Time: 1 ms (99.87%), Space: 43.2 MB (58.57%) - LeetHub

--- counting-bits/counting-bits.java
+++ counting-bits/counting-bits.java
@@ -1,56 +1,76 @@
 class Solution {
-    public int[] countBits(int num) {
-        // non DP solution
-        // int[] result = new int[num+1];
-        // for (int i = 0; i <= num; i++)
-        // {
-        //     int countBits = i;
-        //     int count = 0;
-        //     while (countBits != 0)
-        //     {
-        //         count += (1&countBits);
-        //         countBits >>= 1;
-        //     }
-        //     result[i] = count;
-        // }
-        // return result;
-        
-        // DP solution
-        int[] result = new int[num+1];
-        // looking a lot at this:
-        // https://leetcode.com/problems/counting-bits/discuss/79557/How-we-handle-this-question-on-interview-Thinking-process-%2B-DP-solution. reproduced at bottom of problem
-        // i'm thinking that once we "clear" a power of 2 (e.g. 3, 5, 9, 17), we increase offset by another *2
-        // so we just need to find when we clear a power of 2
-        result[0] = 0;
-        int currentOffset = 1; // represents 2^0
-        for (int i = 1; i <= num; i++)
-        {
-            if (currentOffset * 2 == i)
-                currentOffset *= 2;
-            result[i] = result[i-currentOffset] + 1;
-        }
-        return result;
-    }
+    public int[] countBits(int num) {        
+        // DP solution
+        int[] result = new int[num+1];
+        result[0] = 0;
+        int currentOffset = 1; // represents 2^0
+        for (int i = 1; i <= num; i++)
+        {
+            if (currentOffset * 2 == i)
+                currentOffset *= 2;
+            result[i] = result[i-currentOffset] + 1;
+        }
+        return result;
+    }
 }
-        /*
-        Question:
+        /*
+        Question:
 Given a non negative integer number num. For every numbers i in the range 0 ≤ i ≤ num calculate the number of 1's in their binary representation and return them as an array.
-​
+
 Thinking:
-​
+
 We do not need check the input parameter, because the question has already mentioned that the number is non negative.
-​
+
 How we do this? The first idea come up with is find the pattern or rules for the result. Therefore, we can get following pattern
-​
+
 Index : 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
-​
+
 num : 0 1 1 2 1 2 2 3 1 2 2 3 2 3 3 4
-​
+
 Do you find the pattern?
-​
+
 Obviously, this is overlap sub problem, and we can come up the DP solution. For now, we need find the function to implement DP.
-​
+
 dp[0] = 0;
-​
+
 dp[1] = dp[0] + 1;
-​
+
+dp[2] = dp[0] + 1;
+
+dp[3] = dp[1] +1;
+
+dp[4] = dp[0] + 1;
+
+dp[5] = dp[1] + 1;
+
+dp[6] = dp[2] + 1;
+
+dp[7] = dp[3] + 1;
+
+dp[8] = dp[0] + 1;
+...
+
+This is the function we get, now we need find the other pattern for the function to get the general function. After we analyze the above function, we can get
+dp[0] = 0;
+
+dp[1] = dp[1-1] + 1;
+
+dp[2] = dp[2-2] + 1;
+
+dp[3] = dp[3-2] +1;
+
+dp[4] = dp[4-4] + 1;
+
+dp[5] = dp[5-4] + 1;
+
+dp[6] = dp[6-4] + 1;
+
+dp[7] = dp[7-4] + 1;
+
+dp[8] = dp[8-8] + 1;
+..
+
+Obviously, we can find the pattern for above example, so now we get the general function
+
+dp[index] = dp[index - offset] + 1;
+*/
