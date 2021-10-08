314ba7df964a37fb446a70b07d888e446e29acb2
MatthewPrinz <mattprinz@utexas.edu>
2021-10-07 23:27:20 -0500

    Time: 1 ms (100.00%), Space: 35.3 MB (99.11%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] numSteps = new int[n + 1];
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
+        else if (n == 2)
+            return 2;
+        numSteps[0] = 0;
+        numSteps[1] = 1;
+        numSteps[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            numSteps[i] = numSteps[i-2] + numSteps[i-1];
+        }
+        System.out.println(Arrays.toString(numSteps));
+        return numSteps[n];
+    }
+}
