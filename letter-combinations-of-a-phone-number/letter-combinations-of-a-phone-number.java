66f575da5c3a1b462aed9afcf8f87c94e79848d9
MatthewPrinz <mattprinz@utexas.edu>
2021-10-27 17:27:03 -0500

    Time: 4 ms (44.08%), Space: 39.6 MB (29.80%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,20 +1,21 @@
 class Solution {
+    
     public static final String[] KEYS = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     public List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
         if (digits.length() == 0)
             return result;
-        backtrack(result, "", 0, digits);
+        backtrack(result, digits, "", 0);
         return result;
     }
-    void backtrack(List<String> result, String temp, int offset, String digits) {
-        if (temp.length() == digits.length()) 
+    void backtrack(List<String> result, String digits, String temp, int offset) {
+        if (digits.length() == temp.length())
             result.add(temp);
         else {
-            String letters = KEYS[(digits.charAt(offset) - '2')];
-            for (int i = 0; i < letters.length(); i++) {
-                backtrack(result, temp + letters.charAt(i), offset+1, digits);
-            }  
+            String mapping = KEYS[(digits.charAt(offset) - '2')];
+            for (int i = 0; i < mapping.length(); i++) {
+                backtrack(result, digits, temp + mapping.charAt(i), offset + 1);
+            }
         }
     }
 }
