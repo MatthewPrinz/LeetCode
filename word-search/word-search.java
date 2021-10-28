84cd6329853131f1d149e40ab4836f4748c27e1a
MatthewPrinz <mattprinz@utexas.edu>
2021-10-27 19:36:56 -0500

    Time: 91 ms (85.68%), Space: 37.4 MB (55.56%) - LeetHub

--- word-search/word-search.java
+++ word-search/word-search.java
@@ -4,9 +4,8 @@ class Solution {
             return false;
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (dfs(board, word, i, j, 0)) 
+                if (board[i][j] == word.charAt(0) && dfs(board, word, i, j, 0)) 
                     return true;
-                // printArray(board);
             }
         }
         return false;
@@ -15,13 +14,10 @@ class Solution {
     boolean dfs(char[][] board, String word, int i, int j, int count) {
         if (count == word.length())
             return true;
-        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || count > word.length() ||
-           word.charAt(count) != board[i][j])
+        if (i < 0 || j < 0 || i >= board.length || j >= board[i].length || count > word.length() || word.charAt(count) != board[i][j])
             return false;
         char temp = board[i][j];
         board[i][j] = ' ';
-        // System.out.printf("count is: %d\n", count);
-        // printArray(board);
         boolean result = dfs(board, word, i+1, j, count+1) ||
             dfs(board, word, i-1, j, count+1) ||
             dfs(board, word, i, j+1, count+1) ||
@@ -29,10 +25,4 @@ class Solution {
         board[i][j] = temp;
         return result;
     }
-    
-    // void printArray(char[][] a) {
-    //     for (char[] r : a) {
-    //         System.out.println(Arrays.toString(r));
-    //     }
-    // } 
 }
