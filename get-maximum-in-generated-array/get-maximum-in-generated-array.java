3ed6434d8d9efc11f2b421345027fff8acad83e4
MatthewPrinz <mattprinz@utexas.edu>
2021-11-10 00:57:22 -0600

    Time: 0 ms (100.00%), Space: 36 MB (47.45%) - LeetHub

--- get-maximum-in-generated-array/get-maximum-in-generated-array.java
+++ get-maximum-in-generated-array/get-maximum-in-generated-array.java
@@ -7,11 +7,7 @@ class Solution {
         max = 1;
         dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            if (i % 2 == 0) {
-                dp[i] = dp[i/2];
-            } else {
-                dp[i] = dp[i/2] + dp[(i/2) + 1];
-            }
+            dp[i] = dp[i/2] + dp[(i/2) + 1] * (i % 2);
             if (dp[i] > max)
                 max = dp[i];
         }
