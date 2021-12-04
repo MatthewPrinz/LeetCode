117c8316341e537146c79985a32de2f9ecb88ee2
MatthewPrinz <mattprinz@utexas.edu>
2021-12-04 16:52:30 -0600

    Time: 57 ms (64.05%), Space: 48.4 MB (99.17%) - LeetHub

--- least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -4,15 +4,15 @@ class Solution {
         for (int i : arr) {
             valueToFrequency.put(i, valueToFrequency.getOrDefault(i, 0) + 1);
         }
-        List<Map.Entry<Integer, Integer>> uniques = new ArrayList<>(valueToFrequency.entrySet());
-        uniques.sort(Map.Entry.comparingByValue());
+        Queue<Integer> uniques = new PriorityQueue<>(valueToFrequency.values());
         int result = uniques.size();
         int removed = 0;
         int newK = k;
-        for (Map.Entry<Integer, Integer> me : uniques) {
-            if (me.getValue() - newK <= 0) {
+        while (!uniques.isEmpty()) {
+            int frequency = uniques.poll();
+            if (frequency - newK <= 0) {
                 removed++;
-                newK -= me.getValue();
+                newK -= frequency;
             }
         }
         return result - removed;
