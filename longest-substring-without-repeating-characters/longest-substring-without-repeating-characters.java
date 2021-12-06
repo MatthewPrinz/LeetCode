58a6a525128899e231fb8b00e919b511979133f9
MatthewPrinz <mattprinz@utexas.edu>
2021-12-05 18:36:39 -0600

    Time: 5 ms (81.95%), Space: 38.9 MB (89.31%)
    some bug with leetcode means i do this myself

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
         int max = 0;
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j < s.length(); j++) {
-                if (s.charAt(i) == s.charAt(j) || set.contains(s.charAt(j))) {
-                    break;
-                }
-                else { 
-                    set.add(s.charAt(j));
-                }
-                // System.out.printf("set is: %s\n", set);
+        if (s.length() == 0) {
+            return max;
+        }
+        Map<Character, Integer> charToLastIndex = new HashMap<>();
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (charToLastIndex.containsKey(s.charAt(i))) {
+                j = Math.max(charToLastIndex.get(s.charAt(i)) + 1, j);
             }
-            set.add(s.charAt(i));
-            if (set.size() > max)
-                max = set.size();
-            set.clear();
+            charToLastIndex.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1); 
         }
         return max;
     }
