b84a05fd6eead1ccaf4ef1ac9677496547353dbf
MatthewPrinz <mattprinz@utexas.edu>
2021-12-12 03:29:11 -0600

    Time: 5 ms (99.52%), Space: 42.3 MB (66.08%) - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,58 +1,14 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result = new ArrayList<>();
-        List<Map<Character, Integer>> maps = new ArrayList<>();
+        Map<String, List<String>> sortedToAnagrams = new HashMap<>();
         for (String s : strs) {
-            maps.add(makeMap(s));
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String str = new String(charArray);
+            List<String> anagrams = sortedToAnagrams.getOrDefault(str, new ArrayList<>());
+            anagrams.add(s);
+            sortedToAnagrams.put(str, anagrams);
         }
-        boolean[] processed = new boolean[strs.length];
-        for (int i = 0; i < maps.size(); i++) {
-            if (!processed[i]) {
-                List<String> group = new ArrayList<>();
-                group.add(strs[i]);
-                Map<Character, Integer> map = maps.get(i);
-                for (int j = i; j < maps.size(); j++) {
-                    if (i == j || processed[j])
-                        continue;
-                    boolean isAnagram = true;
-                    Set<Map.Entry<Character, Integer>> set = maps.get(j).entrySet();
-                    // System.out.printf("Now processing: map: %s, set: %s\n", map, set);
-                    if (!(map.isEmpty() && set.isEmpty())) {
-                        if (map.size() == set.size()) {
-                            for (Map.Entry<Character, Integer> me: set) {
-                                if (map.containsKey(me.getKey())) {
-                                    if (!map.get(me.getKey()).equals(me.getValue())) {
-                                        // System.out.printf("val: Not anagrams: %s, %s\n", strs[i], strs[j]);
-                                        isAnagram = false;
-                                        break;
-                                    }
-                                } else {
-                                    // System.out.printf("key: Not anagrams: %s, %s\n", strs[i], strs[j]);
-                                    isAnagram = false;
-                                    break;
-                                }
-                            }
-                        } else {
-                            isAnagram = false;
-                        }
-                    }
-                    if (isAnagram) {
-                        processed[j] = true;
-                        // System.out.printf("strs[%d]=%s is processed, set is: %s, map is: %s\n",j, strs[j], set, map);
-                        group.add(strs[j]);
-                    }
-
-                }
-                result.add(group);
-            }
-        }
-        return result;
-    }
-    public Map<Character, Integer> makeMap(String s) {
-        Map<Character, Integer> charToFrequency = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            charToFrequency.put(s.charAt(i), charToFrequency.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        return charToFrequency;
+        return new ArrayList<>(sortedToAnagrams.values());   
     }
 }
