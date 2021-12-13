06c6dad2f04de8be647eeb86b024ff5f10701ca3
MatthewPrinz <mattprinz@utexas.edu>
2021-12-12 23:39:14 -0600

    Time: 8 ms (91.91%), Space: 39.2 MB (96.16%) - LeetHub

--- count-binary-substrings/count-binary-substrings.java
+++ count-binary-substrings/count-binary-substrings.java
@@ -4,14 +4,16 @@ class Solution {
             return 0;
         int prevRun = 0, curRun = 1, result = 0;
         for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i - 1) == s.charAt(i))
+            if (s.charAt(i - 1) == s.charAt(i)) {
                 curRun++;
+            }
             else {
                 prevRun = curRun;
                 curRun = 1;
             }
-            if (prevRun >= curRun)
+            if (prevRun >= curRun) {
                 result++;
+            }
         }
         return result;
     }
