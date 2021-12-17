983b831253a0dcd23687c668b5928812a3cb1fee
MatthewPrinz <mattprinz@utexas.edu>
2021-12-16 23:11:52 -0600

    Time: 2 ms (75.78%), Space: 39.3 MB (35.90%) - LeetHub

--- add-binary/add-binary.java
+++ add-binary/add-binary.java
@@ -6,22 +6,13 @@ class Solution {
         boolean carry = false;
         StringBuilder result = new StringBuilder();
         while (i >= 0) {
-            int part1 = a.charAt(i) == '1' ? 1 : 0;
-            int part2;
-            if (j >= 0) {
-                part2 = b.charAt(j) == '1' ? 1 : 0;
-            } else {
-                part2 = 0;
-            }
-            int digit = part1 + part2 + (carry ? 1 : 0);
+            int digit = (i >= 0 ? a.charAt(i--) - '0' : 0) + (j >= 0 ? b.charAt(j--) - '0' : 0) + (carry ? 1 : 0);
             carry = false;
             if (digit >= 2) {
                 digit -= 2;
                 carry = true;
             }
             result.insert(0, digit);
-            i--;
-            j--;
         }
         if (carry) {
             result.insert(0, 1);
