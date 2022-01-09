771419614ac97db16db73fb7082d98b983b622e2
MatthewPrinz <mattprinz@utexas.edu>
2022-01-09 01:17:47 -0600

    Time: 2 ms (77.22%), Space: 39.2 MB (35.83%) - LeetHub

---
+++ 67-add-binary/67-add-binary.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        if (a.length() < b.length())
+            return addBinary(b, a);
+        int i = a.length() - 1, j = b.length() - 1;
+        boolean carry = false;
+        StringBuilder result = new StringBuilder();
+        while (i >= 0) {
+            int digit = (a.charAt(i--) - '0') + (j >= 0 ? b.charAt(j--) - '0' : 0) + (carry ? 1 : 0);
+            carry = false;
+            if (digit >= 2) {
+                digit -= 2;
+                carry = true;
+            }
+            result.insert(0, digit);
+        }
+        if (carry) {
+            result.insert(0, 1);
+        }
+        return result.toString();
+    }
+}
