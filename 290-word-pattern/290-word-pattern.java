0752add3c0f6a1e81af6a2458e3ab8d967d59701
MatthewPrinz <mattprinz@utexas.edu>
2022-01-16 03:24:46 -0600

    Time: 1 ms (86.24%), Space: 38.9 MB (9.75%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> patternToS = new HashMap<>();
+        Map<String, Character> sToPattern = new HashMap<>();
+        String[] sSplit = s.split(" ");
+        if (pattern.length() != sSplit.length) {
+            return false;
+        }
+        for (int i = 0; i < sSplit.length; i++) {
+            String check1 = patternToS.put(pattern.charAt(i), sSplit[i]);
+            Character check2 = sToPattern.put(sSplit[i], pattern.charAt(i));
+            if ((check1 != null && !check1.equals(sSplit[i])) || (check2 != null && !check2.equals(pattern.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+// dog cat cat dog
+// aaaa
