843f003b4e7d62defb41886b84c65c332db3c276
MatthewPrinz <mattprinz@utexas.edu>
2022-01-26 23:42:14 -0600

    Time: 123 ms (49.60%), Space: 45.8 MB (52.61%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -9,33 +9,23 @@ class Solution {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 String word = queue.poll();
-                List<String> check = oneLetterDifference(wordList, word);
-                for (String s : check) {
-                    if (!explored.contains(s)) {
-                        if (s.equals(endWord)) {
-                            return ++level;
+                for (int j = 0; j < word.length(); j++) {
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        StringBuilder sb = new StringBuilder(word);
+                        sb.setCharAt(j, c);
+                        String s = sb.toString();
+                        if (dictionary.contains(s) && !explored.contains(s)) {
+                            if (s.equals(endWord)) {
+                                return ++level;
+                            }
+                            explored.add(s);
+                            queue.add(s);
                         }
-                        explored.add(s);
-                        queue.add(s);
                     }
                 }
             }
             level++;
         }
         return 0;
-    }
-    List<String> oneLetterDifference(List<String> words, String word) {
-        List<String> result = new ArrayList<>();
-        for (String s : words) {
-            int numDifferences = 0;
-            for (int i = 0; i < word.length() && numDifferences <= 1; i++) {
-                if (s.charAt(i) != word.charAt(i)) {
-                    numDifferences++;
-                }
-            }
-            if (numDifferences == 1)
-                result.add(s);
-        }
-        return result;
     }
 }
