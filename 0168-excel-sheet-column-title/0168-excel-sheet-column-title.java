2d05f4bda96c0dc84692ec64e4f06484582e5afd
MatthewPrinz <mattprinz@utexas.edu>
2023-08-27 22:03:49 -0600

    Time: 0 ms (100.00%), Space: 39.6 MB (50.90%) - LeetHub

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -1,9 +1,9 @@
 class Solution {
     public String convertToTitle(int columnNumber) {
         StringBuilder result = new StringBuilder();
-        int offset = (int)'A'; 
+        int offset = (int)'A';
         while (columnNumber > 0) {
-            columnNumber -= 1;
+            columnNumber--;
             char letter = (char)((columnNumber % 26) + offset);
             columnNumber /= 26;
             result.insert(0, letter);
