664b81cfc66cc0dc57f73f92123cc7412787e7d7
MatthewPrinz <mattprinz@utexas.edu>
2024-11-11 15:49:19 -0600

    Time: 2 ms (98.17%), Space: 42.5 MB (32.91%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,23 +1,25 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        StringBuilder finalProduct = new StringBuilder();
         int m = num1.length();
         int n = num2.length();
-        if ((num1.equals("0") || num2.equals("0"))) {
+        StringBuilder finalProduct = new StringBuilder();
+        
+        if (((num1.charAt(0) - '0') == 0) || ((num2.charAt(0) - '0') == 0)) {
             return "0";
         }
-        int[] result = new int[m + n];
+        
+        int[] helper = new int[m + n];
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                int product = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                product += result[i + j + 1]; // we've subtracted in the loop, so to get the carry, we need to add 1
-
-                result[i + j + 1] = product % 10;
-                result[i + j] += product / 10;
+                int product = (num1.charAt(i) -'0') * (num2.charAt(j) -'0');
+                product += helper[i + j + 1];
+                
+                helper[i + j] += product / 10;
+                helper[i + j + 1] = product % 10;
             }
         }
-        for (int k : result) {
-            if (finalProduct.length() == 0 && k == 0) {
+        for (int k : helper) {
+            if (k == 0 && finalProduct.length() == 0) {
                 continue;
             }
             finalProduct.append(k);
