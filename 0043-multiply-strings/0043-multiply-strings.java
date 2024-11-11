78201e4cd5fe8e545451df2f5a42a1cc8936a220
MatthewPrinz <mattprinz@utexas.edu>
2024-11-11 16:10:43 -0600

    Time: 3 ms (48.00%), Space: 42.3 MB (55.75%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -12,9 +12,17 @@ class Solution {
         for (int i = m - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
                 int product = (num1.charAt(i) -'0') * (num2.charAt(j) -'0');
-                product += helper[i + j + 1];
                 
+                // 1. this is adding the carry over. we look at index i + j + 1 because the carry over from the last 
+                // iteration of the loop was in that iteration's i + j. Since we have subtracted 1 from j in the for loop,
+                // we must add 1 to i + j to retrieve that carry over.
+                product += helper[i + j + 1]; 
+                
+                // 2. This is where we put carry over
                 helper[i + j] += product / 10;
+                
+                // 3. when we added the carry over in (1), we added it to the product. so we've been accumulating the 
+                // the numbers we get from (2) so we can set this index (as the number that is stored has already been added).
                 helper[i + j + 1] = product % 10;
             }
         }
